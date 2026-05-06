e56d669247697a8f9c234e35377d86d4cdbc8e9b
letuan1910 <leetuaans03@gmail.com>
2026-05-06 15:40:57 +0700

    fe coide

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,4 +1,4 @@
-export const revalidate = 60;
+export const dynamic = 'force-dynamic';
 
 import Link from 'next/link';
 import { Eye, ArrowRight, Calendar, Microscope, ShieldCheck, Users, Truck, Gem, ChevronRight, Award, CheckCircle2 } from 'lucide-react';
