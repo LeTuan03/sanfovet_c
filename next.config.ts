0a1ee396350521c7e88d5ca85b983a03df4609ba
letuan1910 <leetuaans03@gmail.com>
2026-05-06 21:10:57 +0700

    FE fix

--- next.config.ts
+++ next.config.ts
@@ -2,22 +2,26 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: path.resolve(__dirname),
-  },
   images: {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [360, 640, 768, 1024, 1280, 1536, 1920],
     imageSizes: [64, 128, 256, 384, 512],
     minimumCacheTTL: 60 * 60 * 24 * 30,
     remotePatterns: [
-      { protocol: "https", hostname: "*.supabase.co" },
-      { protocol: "https", hostname: "*.supabase.in" },
-      { protocol: "https", hostname: "firebasestorage.googleapis.com" },
-      { protocol: "https", hostname: "lh3.googleusercontent.com" },
-      { protocol: "https", hostname: "img.youtube.com" },
-      { protocol: "https", hostname: "i.ytimg.com" },
+      {
+        protocol: "https",
+        hostname: "sxmzjfxgwhfdfxqpvgyg.supabase.co",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "thu-y-927ab.firebasestorage.app",
+        pathname: "/**",
+      },
     ],
+  },
+  turbopack: {
+    root: path.resolve(__dirname),
   },
 };
 
